4a96822e964b55b5dcd29e3881e42632005d7a99
anthonybroome <anthonybroome87@gmail.com>
2016-08-17 09:41:48 +0100

    Removed duplicate code

--- ditto/core/logger.py
+++ ditto/core/logger.py
@@ -16,27 +16,14 @@ def setup():
 
 
 def log(msg, *args, **kwargs):
-    ditto_logger = logging.getLogger('ditto-cli')
-    msg = str(msg)
-
-    if ditto_logger.mask_values is not None and msg is not None:
-        for to_mask in ditto_logger.mask_values:
-            msg = msg.replace(to_mask, "****")
-
-    ditto_logger.info(Fore.GREEN + msg, *args, **kwargs)
+    log_colour(msg, Fore.GREEN, *args, **kwargs)
 
 
 def vlog(msg, *args, **kwargs):
     ditto_logger = logging.getLogger('ditto-cli')
 
     if ditto_logger.isEnabledFor(logging.DEBUG):
-        msg = str(msg)
-
-        if ditto_logger.mask_values is not None and msg is not None:
-            for to_mask in ditto_logger.mask_values:
-                msg = msg.replace(to_mask, "****")
-
-        ditto_logger.debug(Fore.CYAN + msg, *args, **kwargs)
+        log_colour(msg, Fore.CYAN, *args, **kwargs)
 
 
 def json(json):
@@ -53,17 +40,14 @@ def vjson(json):
 
 
 def error(msg, *args, **kwargs):
-    ditto_logger = logging.getLogger('ditto-cli')
-    msg = str(msg)
-
-    if ditto_logger.mask_values is not None and msg is not None:
-        for to_mask in ditto_logger.mask_values:
-            msg = msg.replace(to_mask, "****")
-
-    ditto_logger.info(Fore.RED + msg, *args, **kwargs)
+    log_colour(msg, Fore.RED, *args, **kwargs)
 
 
 def warn(msg, *args, **kwargs):
+    log_colour(msg, Fore.YELLOW, *args, **kwargs)
+
+
+def log_colour(msg, colour, *args, **kwargs):
     ditto_logger = logging.getLogger('ditto-cli')
     msg = str(msg)
 
@@ -71,7 +55,7 @@ def warn(msg, *args, **kwargs):
         for to_mask in ditto_logger.mask_values:
             msg = msg.replace(to_mask, "****")
 
-    ditto_logger.info(Fore.YELLOW + msg, *args, **kwargs)
+    ditto_logger.info(colour + msg, *args, **kwargs)
 
 
 def ditto(msg):
